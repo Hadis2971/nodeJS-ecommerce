5fe2f39cef49cf8af84d7bac17c6218465b25c48
Albert Hofmann <albhof@email.com>
2018-10-17 13:51:47 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt   = require("bcryptjs");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model("User", userSchema);
+
+module.exports.comparePaswords = function(password, hash, cb){
+    bcrypt.compare(password, hash, (err, isMatch) => {
+        if(err) throw err;
+        else{
+            cb(null, isMatch);
+        }
+    });
+}
